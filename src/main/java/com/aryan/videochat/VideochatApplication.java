d4f6a0d9edf57cef76d345493c7f0b54289542c4
AkBeast007 <aryankashyap0772@gmail.com>
2024-08-08 00:58:49 +0530

    working code

---
+++ src/main/java/com/aryan/videochat/VideochatApplication.java
@@ -0,0 +1,45 @@
+package com.aryan.videochat;
+
+import com.aryan.videochat.user.User;
+import com.aryan.videochat.user.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class VideochatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VideochatApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			UserService service
+	)
+	{
+		return args -> {
+			service.register(User.builder()
+					.username("John")
+					.email("john@gmail.com")
+					.password("123")
+					.build());
+
+			service.register(User.builder()
+					.username("Doe")
+					.email("doe@gmail.com")
+					.password("345")
+					.build());
+
+			service.register(User.builder()
+					.username("Anny")
+					.email("anny@gmail.com")
+					.password("789")
+					.build());
+		};
+	}
+
+
+}
---
+++ src/test/java/com/aryan/videochat/VideochatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.videochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideochatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
